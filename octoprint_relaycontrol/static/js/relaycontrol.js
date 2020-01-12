42b706189274873fad0f725f7e5223ac87c12c3c
mgourde <mgourde@users.noreply.github.com>
2020-01-12 15:31:40 -0500

    Add files via upload

---
+++ octoprint_relaycontrol/static/js/relaycontrol.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-Relaycontrol
+ *
+ * Author: Martial Gourde
+ * License: AGPLv3
+ */
+$(function() {
+    function RelaycontrolViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: RelaycontrolViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_relaycontrol, #tab_plugin_relaycontrol, ...
+        elements: [ /* ... */ ]
+    });
+});
